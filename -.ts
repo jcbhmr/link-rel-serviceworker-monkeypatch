35f03949ed58834578abad1dfac9bc132f893671
Jacob Hummer <61068799+jcbhmr@users.noreply.github.com>
2022-07-03 01:29:50 +0000

    Rework into TS

---
+++ -.ts
@@ -0,0 +1,62 @@
+/**
+ * 🏗️ Inspects the document for a <link rel="serviceworker"> and
+ * registers it
+ *
+ * The first <link rel="serviceworker"> wins, regardless of its
+ * validity. Any others will emit an error event on themselves.
+ *
+ * The scope="" and type="" attributes are used to build the
+ * options object that is passed to serviceWorker.register()
+ * function. Any errors in registration will be passed up as
+ * error events to the relevant <link> element.
+ *
+ * This polyfill does NOT respond to updates the the <link>
+ * tag after the DOMContentLoaded event. It is a single-execution
+ * function. You can trigger it again manually if you so choose.
+ */
+export default (
+  node: Node = document,
+  serviceWorker: ServiceWorkerContainer = navigator.serviceWorker
+) => {
+  // Extract only the first one to deal with, emit an error
+  // event on all the other ones
+  const [first, ...rest] = [...node.querySelectorAll("link")].filter((link) =>
+    link.relList.contains("serviceworker")
+  );
+
+  // Here we .dispatchEvent() so that all these invalid link elements
+  // get the message that they, indeed, invalid
+  for (const link of rest) {
+    const error = new Error(`<link rel="serviceworker"> tag already declared!`);
+    link.dispatchEvent(
+      new ErrorEvent("error", { error, message: error.message })
+    );
+  }
+
+  // Might have been a 0-length array!
+  if (first == null) {
+    return;
+  }
+
+  // Now we deal with that original <link> element. We use promises here
+  // instead of async-await because they mesh with this being a sync function.
+  // These possibly-undefined function calls are OK since if they are indeed
+  // undefined, the option won't be interpreted by the .register() function!
+  serviceWorker
+    .register(link.href, {
+      scope: link.getAttribute("scope"),
+      type: link.getAttribute("type"),
+    })
+    .then((registration) => {
+      // Make sure to notify the <link> element that it worked!
+      link.dispatchEvent(new CustomEvent("load", { detail: registration }));
+    })
+    .catch((error) => {
+      // Something went wrong, so better forward that on to the <link> element
+      link.dispatchEvent(
+        new ErrorEvent("error", { error, message: error.message })
+      );
+    });
+
+  // Don't return anything since this is a side-effect-only function
+};
